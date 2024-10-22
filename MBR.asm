7aefe8e357ee2c5dd4a5a4963efb8e6ffa379cda
cmpzx <dsdds9073@gmail.com>
2024-10-22 13:02:38 +0200

    Create MBR.asm
    creating MBR.asm

---
+++ MBR.asm
@@ -0,0 +1,22 @@
+[bits 16]
+[org 0x7c00]
+
+KERNELOFFSET equ 0x1000
+
+;bios boot drive
+mov [BOOT_DRIVE], dl
+
+; stack
+mov bp, 0x9000
+mov sp,bp
+
+call lKernel
+
+[bits 16]
+lKernel:
+  mov bx, KERNELOFFSET ; -> destination
+  mov dh, 2 ; -> num sectors
+  mov dl, [BOOT_DRIVE] ; -> disk
+
+
+// Todo Diskloader
